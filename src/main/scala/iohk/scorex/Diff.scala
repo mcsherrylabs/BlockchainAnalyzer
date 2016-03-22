69ec940210e6dba0d33e4684a75896ad57ce1dbc
catena <catena@protonmail.com>
2016-03-22 19:23:10 +0300

    Linear difficulty

--- src/main/scala/iohk/scorex/Diff.scala
+++ src/main/scala/iohk/scorex/Diff.scala
@@ -21,60 +21,60 @@ object Diff extends App {
   val Ra = 200
 
 
-  //  def hashRate(i: Int): BigInt = BigInt(1000)
-  //  def hashRate(i: Int, prev: HashRate): BigInt = prev * 110 / 100
-//  def hashRate(i: Int, prev: HashRate): BigInt = if (i % 2 == 1) R else R + Ra
+//    def hashRate(i: Int): BigInt = BigInt(1000)
+//    def hashRate(i: Int, prev: HashRate): BigInt = prev * 110 / 100
+//    def hashRate(i: Int, prev: HashRate): BigInt = if (i % 2 == 1) R else R + Ra
   def hashRate(i: Int, prev: HashRate): BigInt = prev * (900 + Random.nextInt(204)) / 1000
 
-  def simulate(i: Int, acc: Seq[(Int, Difficulty, HashRate, Long)], maxI: Int): Seq[(Int, Difficulty, HashRate, Long)] = if (i < maxI) {
+  def simulate(i: Int, acc: Seq[(Int, Difficulty, HashRate, FiniteDuration)], maxI: Int): Seq[(Int, Difficulty, HashRate, FiniteDuration)] = if (i < maxI) {
     val R = hashRate(i, acc.head._3)
-    println(R)
-    val realDuration = (desired.toMillis * acc.head._2 / R).toInt.millis
-    val newDiff = diffControl.diff(acc.take(5).map(_._2), realDuration, desired)
-    simulate(i + 1, (i, newDiff, R, realDuration.toMillis) +: acc, maxI)
+    val newDiff = diffControl.diff(acc.take(5).map(a => a._2 -> a._4), desired)
+    val realDuration = (desired.toMillis * newDiff / R).toInt.millis
+    simulate(i + 1, (i, newDiff, R, realDuration) +: acc, maxI)
   } else acc
 
-  val res = simulate(1, Seq((0, BigInt(1000), BigInt(1000), 0L)), 1000).reverse
-  val file = new File("D-B-rand.txt")
+  val res = simulate(1, Seq((0, BigInt(1000), BigInt(1000), desired)), 1000).reverse
+  val file = new File("data/Bitcoin/random.txt")
   val bw = new BufferedWriter(new FileWriter(file))
-  res.foreach(r => bw.write(r._1 + " " + r._2 + " " + r._3 + " " + r._4 + "\n"))
-  bw.write((res.map(_._4).sum / res.length) + "\n")
+  res.foreach(r => bw.write(r._1 + " " + r._2 + " " + r._3 + " " + r._4.toMillis + "\n"))
+  bw.write((res.map(_._4.toMillis).sum / res.length) + "\n")
   bw.close()
 
 
 }
 
 sealed trait DifficultyControl {
-  def diff(lastDiffs: Seq[Difficulty], real: FiniteDuration, desired: FiniteDuration): BigInt
+  def diff(lastDiffs: Seq[(Difficulty, FiniteDuration)], desired: FiniteDuration): BigInt
 }
 
 class BitcoinDifficultyControl extends DifficultyControl {
-  override def diff(lastDiffs: Seq[BigInt], real: FiniteDuration, desired: FiniteDuration): BigInt = {
-    lastDiffs.head * desired.toMillis / real.toMillis
+  override def diff(lastDiffs: Seq[(Difficulty, FiniteDuration)], desired: FiniteDuration): BigInt = {
+    lastDiffs.head._1 * desired.toMillis / lastDiffs.head._2.toMillis
   }
 }
 
 class LinearDifficultyControl extends DifficultyControl {
-  override def diff(lastDiffs: Seq[Difficulty], real: FiniteDuration, desired: FiniteDuration): BigInt = {
-    if(lastDiffs.size > 2) {
-      val data = (1 to lastDiffs.size).map(i => i->lastDiffs(i)).toMap
+  override def diff(lastDiffs: Seq[(Difficulty, FiniteDuration)], desired: FiniteDuration): BigInt = {
+    if (lastDiffs.size > 2) {
+      val realDiffs: Seq[BigInt] = lastDiffs.map(l => l._1 * desired.toMillis / l._2.toMillis).toSeq
+      val data: Seq[(Int, Difficulty)] = (1 to realDiffs.size).map(i => i -> realDiffs(i))
       interpolate(data)(data.map(_._1).max + 1)
-    } else lastDiffs.head
+    } else lastDiffs.head._1
   }
 
   //y = a + bx
-  private def interpolate(data:Map[Int, BigInt]): (Int) => BigInt = {
+  private def interpolate(data: Seq[(Int, BigInt)]): (Int) => BigInt = {
     val xy: Iterable[BigInt] = data.map(d => d._1 * d._2)
-    val x: Iterable[Int] = data.map(d => d._1 )
-    val x2: Iterable[Int] = data.map(d => d._1* d._1)
+    val x: Iterable[Int] = data.map(d => d._1)
+    val x2: Iterable[Int] = data.map(d => d._1 * d._1)
     val y: Iterable[BigInt] = data.map(d => d._2)
     val xyMean = xy.sum / xy.size
     val xMean = x.sum / x.size
     val x2Mean = x2.sum / x2.size
     val yMean = y.sum / y.size
 
-    val b = (xyMean-xMean*yMean) / (x2Mean - xMean*xMean)
-    val a = yMean - b*xMean
+    val b = (xyMean - xMean * yMean) / (x2Mean - xMean * xMean)
+    val a = yMean - b * xMean
     (point: Int) => {
       a + b * point
     }
